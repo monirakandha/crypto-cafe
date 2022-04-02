5657145b72c6432dc79e8504a3c533aab37eaaf7
monirhosenakanda <monirhosenakanda@gmail.com>
2022-04-02 13:58:34 +0600

    coin page complete with dynamic url

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Coins from './components/Coins/Coins'
 import Contact from './components/Contact/Contact'
 import Header from './components/Header/Header'
 import Homepage from './components/Homepage/Homepage'
+import Footer from'./components/Footer/Footer'
+import CoinDetails from './components/CoinDetails/CoinDetails'
 
 const App = () => {
   return (
@@ -13,9 +15,11 @@ const App = () => {
       <Routes>
         <Route path='/' element={<Homepage/>}></Route>
         <Route path='/coins' element={<Coins/>}></Route>
+        <Route path='/coin-details/:id' element={<CoinDetails/>}></Route>
         <Route path='/about' element={<About/>}></Route>
         <Route path='/contact' element={<Contact/>}></Route>
       </Routes>
+      <Footer/>
     </div>
   )
 }
---
+++ src/components/CoinDetails/CoinDetails.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CoinDetails = () => {
+    return (
+        <div>
+            <h1>Coin Detials Page</h1>
+        </div>
+    );
+};
+
+export default CoinDetails;
